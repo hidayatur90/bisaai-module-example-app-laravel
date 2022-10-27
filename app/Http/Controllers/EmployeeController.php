f4e28a9d1aaecb3fa5c2701361e3ee3278d766fd
Muhammad Hidayatur Rahman <hidabaik@gmail.com>
2022-10-27 09:52:54 +0700

    9:52

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,88 +8,11 @@ use Illuminate\Support\Facades\DB;
 
 class EmployeeController extends Controller
 {
-    // //index
-    // public function index()
-    // {
-    //     return view('indexEmployee');
-    // }
-
-    // public function loopingExample()
-    // {
-    //     $datas = [];
-    //     for ($i=0; $i < 10; $i++) { 
-    //         array_push($datas,  $i);
-    //     }
-    //     return view('looping', ['datas' => $datas]);
-    // }
-
-    // public function returnGenderValue()
-    // {
-    //     // data 1
-    //     $males = ["Robby", "Jaya", "Kukun"];
-
-    //     // data 2
-    //     $females = ["Jenny", "Lisa", "Rose"];
-
-    //     return view('employeeGender', compact('males', 'females'));
-    // }
 
     public function get_all_employees()
     {
         $employees = Employees::all();
         return view('indexEmployee', ['employees' => $employees]);
-    }
-
-    public function store_employee(Request $request)
-    {
-        $this->validate($request,[
-    		'firstName' => 'required|string',
-    		'lastName' => 'required|string',
-    		'gender' => 'required|string',
-    		'age' => 'integer']
-        );
-
-        Employees::create([
-            'firstName' => $request->firstName,
-            'lastName' => $request->lastName,
-            'gender' => $request->gender,
-            'age' => $request->age
-        ]);
-
-        return redirect('/');
-
-    }   
-
-    public function get_employee_by_id($id)
-    {
-        $employee = Employees::find($id);
-        return view('editEmployee', ['employee' => $employee]);
-    }
-
-    public function update_employee(Request $request, $id)
-    {
-        $this->validate($request,[
-    		'firstName' => 'required|string',
-    		'lastName' => 'required|string',
-    		'gender' => 'required|string',
-    		'age' => 'integer']
-        );
-
-        Employees::where('id', $id)
-        ->update([
-            'firstName' => $request->firstName,
-            'lastName' => $request->lastName,
-            'gender' => $request->gender,
-            'age' => $request->age
-        ]);
-
-        return redirect('/');
-    }
-
-    public function delete_employee($id)
-    {
-        Employees::where('id', $id)->delete();
-        return redirect('/');
     }
 
 }
--- resources/views/indexEmployee.blade.php
+++ resources/views/indexEmployee.blade.php
@@ -6,8 +6,36 @@
         <h3>Daftar Karyawan</h3>
         <p>PT. Bisa Artifisial Indonesia</p>
     </div>
+    <div>
+        <a class="btn btn-primary mb-2" href="addEmployee">Tambah</a>
+    </div>
+    <table class="table">
+        <thead class="table-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Update At</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Usia</th>
+                <th scope="col">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($employees as $employee)
+            <tr>
+                <td>{{ $loop->iteration . '.' }}</td>
+                <td>{{ $employee->firstName . ' ' . $employee->lastName }}</td>
+                <td>{{ $employee->updated_at }}</td>
+                <td>{{ $employee->gender }}</td>
+                <td>{{ $employee->age }}</td>
+                <td>
+                    <a class="btn btn-warning" href="editEmployee/{{ $employee->id }}">Edit</a>
+                    <a class="btn btn-danger" href="deleteEmployee/{{ $employee->id }}" 
+                        onclick="return confirm('Yakin ingin menghapus data ini?')">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
-
-
-
