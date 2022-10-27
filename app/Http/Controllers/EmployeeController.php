27accef1be62698ae4461d7d6df04dbcc9229822
Muhammad Hidayatur Rahman <hidabaik@gmail.com>
2022-10-27 10:08:36 +0700

    10:08

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Employees;
-use Illuminate\Support\Facades\DB;
 
 class EmployeeController extends Controller
 {
@@ -13,6 +12,58 @@ class EmployeeController extends Controller
     {
         $employees = Employees::all();
         return view('indexEmployee', ['employees' => $employees]);
+    }
+
+    public function store_employee(Request $request)
+    {
+        $this->validate($request,[
+    		'firstName' => 'required|string',
+    		'lastName' => 'required|string',
+    		'gender' => 'required|string',
+    		'age' => 'integer']
+        );
+
+        Employees::create([
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
+            'gender' => $request->gender,
+            'age' => $request->age
+        ]);
+
+        return redirect('/');
+
+    }   
+
+    public function get_employee_by_id($id)
+    {
+        $employee = Employees::find($id);
+        return view('editEmployee', ['employee' => $employee]);
+    }
+
+    public function update_employee(Request $request, $id)
+    {
+        $this->validate($request,[
+    		'firstName' => 'required|string',
+    		'lastName' => 'required|string',
+    		'gender' => 'required|string',
+    		'age' => 'integer']
+        );
+
+        Employees::where('id', $id)
+        ->update([
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
+            'gender' => $request->gender,
+            'age' => $request->age
+        ]);
+
+        return redirect('/');
+    }
+
+    public function delete_employee($id)
+    {
+        Employees::where('id', $id)->delete();
+        return redirect('/');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -14,46 +14,6 @@ use App\Http\Controllers\EmployeeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/employee', [
-//     EmployeeController::class, 'index'
-// ]);
-
-// Route::get('/looping', [
-//     EmployeeController::class, 'loopingExample'
-// ]);
-
-// Route::get('/gender', [
-//     EmployeeController::class, 'returnGenderValue'
-// ]);
-
 Route::get('/', [
     EmployeeController::class, 'get_all_employees'
 ]);
-
-Route::get('/addEmployee', function () {
-    return view('addEmployee');
-});
-
-Route::patch(
-    '/addEmployee/store', 
-    [EmployeeController::class, 'store_employee']
-);
-
-Route::get(
-    '/editEmployee/{id}', 
-    [EmployeeController::class, 'get_employee_by_id']
-);
-
-Route::patch(
-    '/editEmployee/update/{id}', 
-    [EmployeeController::class, 'update_employee']
-);
-
-Route::get(
-    '/deleteEmployee/{id}', 
-    [EmployeeController::class, 'delete_employee']
-);
